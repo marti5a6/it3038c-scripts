72ddfc7229d53ab2264669e5c456410c37e5dc78
marti5a6 <marti5a6@mail.uc.edu>
2022-03-08 18:29:38 -0500

    node test

---
+++ node/test.js
@@ -0,0 +1,9 @@
+var path = require("path");
+
+var hello = "Hello from Node JS variable!";
+console.log(hello);
+
+console.log("Using PATH module:");
+console.log('Hello from file ${path.basename(__filename)}');
+
+console.log('Process args: ${process.argv}');
