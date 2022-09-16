1119714572c36b2f8e70edc9d82cfe531f1008aa
marti5a6 <marti5a6@mail.uc.edu>
2022-09-16 13:30:06 -0400

    Lab3 Script + Output

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,19 @@
+﻿function getIP {
+(Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+$IP = (getIP)
+
+$USER = ($env:USERNAME)
+
+$HOSTNAME = (hostname)
+
+$PSVersion = ($PSVersionTable.PSVersion)
+
+$DATE = (Get-Date -Format "dddd, MMMM d, yyyy")
+
+$BODY=("This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. PowerShell Version $PSVersion. Today's date is $DATE.")
+
+Write-Output($BODY) | Out-File -FilePath C:\it3038c-scripts\powershell\Lab3.txt
+
+notepad.exe C:\it3038c-scripts\powershell\Lab3.txt
