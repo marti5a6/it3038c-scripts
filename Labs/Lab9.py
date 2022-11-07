9e6514aaad44f61c0e7d3bc94d145157fe1c321d
Tony <marti5a6@mail.uc.edu>
2022-11-07 00:51:46 -0500

    fix loop + add delay

--- Labs/Lab9.py
+++ Labs/Lab9.py
@@ -1,48 +1,61 @@
 import json
 import requests
-
-valid = False
+import time
 
 def continueSearch():
     continueValid = False
+    global cont
 
     while not continueValid:
         print('Would you like to search again? [Y/N] ')
         continueAnswer = input()
 
         if continueAnswer.casefold() == 'y':
-            valid = False
+            cont = True
             continueValid = True
         elif continueAnswer.casefold() == 'n':
             print('Terminating... ')
-            valid = False
+            time.sleep(2)
+            cont = False
             continueValid = True
         else:
             print('Invalid response. ')
             continueValid = False
 
-while not valid:
-    print('Filter by color? [Y/N]')
-    filterAnswer = input()
+cont = True
 
-    if filterAnswer.casefold() == 'y':
-        print('Please enter a color: ')
-        color = input()
-        valid = True
-    elif filterAnswer.casefold() == 'n':
-        print('Displaying all widgets: ')
-        color = ''
-        valid = True
-    else:
-        print('Invalid response. ')
-        valid = False
+while cont:
+    valid = False
 
-r = requests.get('http://localhost:3000/%s' % color)
+    while not valid:
+        print('Filter by color? [Y/N]')
+        filterAnswer = input()
 
-try:
-    data = r.json()
-    print(data)
-    continueSearch()
-except:
-    print('No widgets matching', color, 'were found.')
-    continueSearch()
+        if filterAnswer.casefold() == 'y':
+            print('Please enter a color: ')
+            color = input()
+            valid = True
+        elif filterAnswer.casefold() == 'n':
+            print('Displaying all widgets: ')
+            color = ''
+            valid = True
+        else:
+            print('Invalid response. ')
+            time.sleep(2)
+            valid = False
+
+    r = requests.get('http://localhost:3000/%s' % color)
+
+    try:
+        data = r.json()
+
+        for i in data:
+            print(i['name'], 'is', i['color'])
+
+        time.sleep(2)
+        
+        continueSearch()
+    except:
+        print('No widgets matching', color, 'were found.')
+        time.sleep(2)
+        continueSearch()
